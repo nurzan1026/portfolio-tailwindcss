dad3d851d8ee307f84e7d758a61d15b79cc25efa
M NUR ICHSAN IN <muh.iksank102602@gmail.com>
2025-02-01 19:58:09 +0800

    menambahkan menu navbar

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
+// todo === Menambahkan gambar di tab browser
 const gambarLink = document.createElement("link");
 gambarLink.rel = "icon";
 gambarLink.href = "/img/profile-iksan.png";
 document.head.appendChild(gambarLink);
+
+// ? ===== hamburger menu =====
+
+const hamburgerMenu = document.querySelector("#menu-btn");
+const navMenu = document.querySelector("#navbar-menu");
+
+
+hamburgerMenu.addEventListener("click", function () {
+  hamburgerMenu.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// ! ===== navbar fixed =====
+
+window.onscroll = function () {
+  const navbar = document.querySelector("header");
+  const sticky = navbar.offsetTop;
+
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,11 @@ module.exports = {
     function ({ addComponents }) {
       addComponents({
         ".btn-link": {
-          "@apply flex items-center space-x-2 text-dark border border-primary py-5 px-5 rounded-md w-full h-10 justify-center hover:bg-yellow-300 hover:text-slate-800 transition duration-300 ease-in-out":
+          "@apply flex items-center space-x-2 text-dark border border-primary py-5 px-5 md:py-6 md:px-6 rounded-md w-full h-10 justify-center hover:bg-kuning hover:text-slate-800 transition duration-300 ease-in-out":
+            "",
+        },
+        ".hamburger-menu": {
+          "@apply w-[30px] h-[2px] my-2 block bg-primary origin-top-left transition duration-500 ease-in-out rounded-md":
             "",
         },
       });
